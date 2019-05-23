a82e3f54e773a4da619379bcc72112401ab8b561
yuji_namikawa <yuji_namikawa@dwango.co.jp>
2019-05-23 18:47:43 +0900

    base

---
+++ README.md
@@ -0,0 +1,38 @@
+# nsen design mock
+
+## URL
+https://ug-namikawa.github.io/portfolio/public/
+
+## 使用方法
+
+### 各自のリポジトリを作成する
+
+1. リポジトリをForkする
+
+2. Forkしたリポジトリをローカルにダウンロードする
+
+  ```Shell
+  $ git clone URLを入れる
+  ```
+
+3. 設定する
+
+  ```Shell
+  // ダウンロードしたディレクトリに移動し、npm installする
+  $ npm install
+  ```
+
+3. 動かす
+
+  ```Shell
+  $ npm start
+  ```
+
+
+# 利用可能なもの
+
+- html
+- ejs
+- css
+- scss
+- js
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var autoprefixer = require('gulp-autoprefixer');
+var del = require("del");
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var babel = require("gulp-babel");
+var ejs = require('gulp-ejs');
+
+gulp.task('default', () => {
+  console.log('complete');
+});
+
+gulp.task('clean', (callback) => {
+  return del(['dist'], callback);
+});
+
+gulp.task('build', ['clean'], (callback) => {
+  return runSequence(
+    ['html', 'image', 'scss', 'babel', 'ejs'],
+    callback
+  );
+});
+
+gulp.task('html', () => {
+  return gulp.src('./assets/**/*.html')
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('ejs', () => {
+  return gulp.src('assets/**/!(_)*.ejs')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }))
+    .pipe(ejs({ msg: ''}, {}, { ext: '.html' }))
+    .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('image', () => {
+  return gulp.src('./assets/**/*.+(jpg|png|gif|svg|ico)')
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('babel', () => {
+  return gulp.src('./assets/**/*.js')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('scss', () => {
+  return gulp.src('./assets/**/*.scss')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }))
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie 11', 'android 4'],
+      remove: false
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('browser-sync', () => {
+  return browserSync({
+    server: {
+      baseDir: './dist/'
+    },
+    open: 'external',
+  });
+});
+
+gulp.task('watch', ['build', 'browser-sync'], () => {
+  gulp.watch('assets/**/*.scss', ['scss']);
+  gulp.watch('./assets/**/*.+(jpg|png|gif|svg|ico)', ['image']);
+  gulp.watch('./assets/**/*.html', ['html']);
+  gulp.watch('assets/**/*.ejs', ['ejs']);
+  gulp.watch('./assets/**/*.js', ['babel']);
+  gulp.watch(['./dist/**/*.html', './dist/**/*.css', './dist/**/*.js', './dist/**/*.+(jpg|png|gif|svg|ico)'], () => {
+    browserSync.reload();
+  });
+});
